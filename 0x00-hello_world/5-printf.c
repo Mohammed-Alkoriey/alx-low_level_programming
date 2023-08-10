7acc6153fc1f66b05c7547b54de78aff36ada385
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-10 13:40:41 -0700

    update .gitignore

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this is the program's main function
+ *Return: 0 to end the program
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}

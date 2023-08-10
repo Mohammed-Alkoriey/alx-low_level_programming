6a64757b7a7a3f15f8099359b82e20aa32e5927d
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-10 13:32:20 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - this is the program's main function
+ *Return: 0 to end the program
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}

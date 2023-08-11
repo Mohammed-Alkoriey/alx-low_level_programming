24f97ea18362bfba22dce08460afd071e9f7c277
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-11 12:55:29 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - prints all possible diffrent combinations of three digits.
+ *
+ *Return: 0 success
+ */
+int main(void)
+{
+  int iFirst, iSecond;
+for (iFirst = 0; iFirst <= 99; iFirst++)
+{
+for (iSecond = iFirst + 1; iSecond <= 99; iSecond++)
+{
+if (iFirst == iSecond)
+{
+continue;
+}
+ putchar((iFirst / 10) + 48);
+ putchar((iFirst % 10) + 48);
+ putchar(' ');
+ putchar((iSecond / 10) + 48);
+ putchar((iSecond % 10) + 48);
+if (iFirst == 98 && iSecond == 99)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

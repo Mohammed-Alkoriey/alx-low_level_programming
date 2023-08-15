7470b3f1910b49ed7e993ce80cc14a1b26af1e81
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-15 14:50:40 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,7 +25,7 @@ continue;
 }
 else if (iNumber > 9)
 {
-if (iNumber % 9 == 0)
+if (y == 0)
 {
 _putchar(32);
 _putchar((iNumber / 10) + 48);
@@ -47,16 +47,13 @@ _putchar((iNumber % 10) + 48);
 _putchar(44);
 continue;
 }
-else if (iNumber % 9 == 0)
-{
-if (iNumber <= 9)
+else if (y == 9 && x != 9)
 {
 _putchar(32);
 _putchar(32);
 _putchar((iNumber % 10) + 48);
 _putchar(44);
 continue;
-}
 }
 _putchar(32);
 _putchar(32);

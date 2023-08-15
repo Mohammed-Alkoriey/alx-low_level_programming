09796325f4ece48a58fbe524b08f4a52fa3c5eb2
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-15 04:47:10 -0700

    0. _putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- *_putchar - writes the character c to the stdout 
- *@c: the character to be writen
+ * _putchar - writes the character c to the stdout 
+ * @c: the character to be writen
  *
- *Return: 1 on success
+ * Return: 1 on success
  */
 
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 }

f53128c0cdd27d927954619eda999ff86df37abc
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-17 02:59:03 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints if the number is negative or positive
+ * Return: 0 if success
+ */
+int main()
+{
+  int i;
+
+  i = 9;
+  positive_or_negative(i);
+
+  return (0);
+}

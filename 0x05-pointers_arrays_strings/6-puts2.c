a42b970bbf58594458452b55891787e0805436cc
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-21 13:46:08 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print half of the string
+ * @str: the string that needs to be halfed
+ * Return: nothing
+ */
+
+void puts2(char *str);
+{
+int i = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] % 2 ==  0)
+{
+_putchar(s[i]);
+}
+}
+_putchar('\n');
+}

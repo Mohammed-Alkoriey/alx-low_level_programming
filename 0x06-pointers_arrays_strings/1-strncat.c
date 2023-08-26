28669edfa35b4ea798d045799425901a42c7ba0b
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-26 15:04:53 +0300

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * *_strcat  - concatenates two strings
+ * *_strncat  - concatenates a string with a number of characters from the other string
  * @dest: the string that the source is gonna be appended to
  * @src: the source that's gonna be appended to the dest string
+ * @n: the number of the characters
  * Return: dest after appending
  */
 
-char *_strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
 int x, m;
 
@@ -15,7 +17,7 @@ x = 0;
 while (dest[x])
 x++;
 
-for (m = 0; src[m]; m++)
+for (m = 0; m < n; m++)
 {
 dest[x++] = src[m];
 }

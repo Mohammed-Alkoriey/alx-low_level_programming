5eadbd50f7733669e984be23b1ce1196297b54a4
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-28 14:39:15 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,6 +5,18 @@
  * @str: the string that's gonna be capitalized
  * Return: the the capitalized string
  */
+char *cap_string(char *);
+
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
 
 char *cap_string(char *str)
 {
@@ -16,7 +28,7 @@ if (*(str + 0) >= 97 && *(str + 0) <= 122)
 }
 while (*(str + i))
 {
-if (*(str + i) == 32 || *(str + i) == 9 || *(str + i) == 44 || *(str + i) == 59 || *(str + i) == 46 || *(str + i) == 33 || *(str + i) == 63 || *(str + i) == 34 || *(str + i) == 40 || *(str + i) == 41 || *(str + i) == 123 || *(str + i) == 126 || *(str + i) == 10)
+if (*(str + 1) == (32 || 9 || 44 || 59 || 46 || 33 || 63 || 34 || 40 || 41 || 123 || 125 || 10))
 {
 if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
 {

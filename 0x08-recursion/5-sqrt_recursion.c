e8d0b072510e614c4b9cc8e7e917e5828f6da2a4
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-08-31 22:41:14 +0300

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calls sqrt function
+ * @n: is the number
+ * Return: the square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+return (sqr(n, 1));
+}
+
+/**
+ * sqr - a function to find the square root
+ * @x: this is the number
+ * @y: a number to help us find the square root
+ * Return: the square root
+ */
+
+
+int sqr(int x, int y)
+{
+if (y * y == x)
+{
+return (y);
+}
+else if (y * y < x)
+{
+return (sqr(x, y + 1));
+}
+else
+{
+return (-1);
+}
+}
+}

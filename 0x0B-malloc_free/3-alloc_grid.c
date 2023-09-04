c26add505f5f5e53796709b802ce9a270acf92a0
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-05 00:49:42 +0300

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *alloc_grid  - creates a two dimentional array
+ * @width: the width of the array
+ * @height: the heigth of the array
+ * Return: null if fail and the array if success
+ */
+
+int **alloc_grid(int width, int height)
+{
+int **arr;
+int i = 0, x = 0;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+else
+{
+arr = malloc(sizeof(char) *  height);
+if (arr == NULL)
+{
+return (NULL);
+}
+else
+{
+while (i < height)
+{
+arr[i] = malloc(sizeof(char) *  width);
+while (x < width)
+{
+arr[i][x] = 0;
+x++;
+}
+i++;
+}
+}
+}
+return (arr);
+free(arr);
+}

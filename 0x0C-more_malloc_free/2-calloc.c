94f085241936f7b59174f8338ebbe9c1afee975e
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-08 23:56:27 +0300

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr;
+void *arr;
 unsigned int i = 0;
 if (nmemb == 0 || size == 0)
 return (NULL);
@@ -22,7 +22,7 @@ else
 {
 for (i = 0; i < nmemb; i++)
 {
-arr[i] = 0;
+arr[i] = 48;
 }
 return (arr);
 }

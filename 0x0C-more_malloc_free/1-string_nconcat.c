7070e71fde48fdae4bcd07efd89cdf203f442f15
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-08 23:28:23 +0300

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,47 +9,25 @@
  * @n: number of characters
  * Return: null if fail and the string if success
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Best ", "bee", 30);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
-}
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
 unsigned int len1 = 0, len2 = 0, i = 0, x = 0;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
 while (s1[len1])
-{
 len1++;
-}
 while (s2[len2])
-{
 len2++;
-}
 if (n >= len2)
-{
 n = len2;
-}
 str = malloc(sizeof(char) * (len1 + len2 + 1));
 if (str == NULL)
-{
 return (NULL);
-}
+else
+{
 for (i = 0; i < len1; i++)
 {
 str[i] = s1[i];
@@ -60,5 +38,6 @@ str[i] = s2[x];
 x++;
 }
 str[len1 + len2] = '\0';
+}
 return (str);
 }

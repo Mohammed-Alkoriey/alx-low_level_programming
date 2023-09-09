46f96083953c5d70e1cd1c15e4ccc2022e13eb8c
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-09 14:25:47 +0300

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,48 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements
- * @size: size of the array
+ * *array_range - creates an array of integers
+ * @min: the starting point of the array
+ * @max: the ending point of the array
  * Return: null if fail and the array if success
  */
-int *array_range(int min, int max);
-
-void simple_print_buffer(int *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int *a;
-
-    a = array_range(0, 10);
-    simple_print_buffer(a, 11);
-    free(a);
-    return (0);
-}
 
 int *array_range(int min, int max)
 {

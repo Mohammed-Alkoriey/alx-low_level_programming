1af8e00cd2859a932b4709db6a7990d00641aa57
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-21 23:51:31 +0300

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+
+/**
+ * _strlen - computes the length of a string
+ * @s: the string that needs to be computed
+ * Return: the sting's length
+ */
+
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; *s != '\0'; s++)
+{
+++i;
+}
+return (i);
+}
+
+
+/**
+ * print_list - prints the gevin head's list
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			x++;
+		}
+		else
+		{
+			printf("[%d] %s\n", _strlen(h->str), h->str);
+			h = h->next;
+			x++;
+		}
+
+	}
+	return (x);
+}

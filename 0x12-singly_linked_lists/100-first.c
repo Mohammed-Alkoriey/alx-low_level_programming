36e6b20163f9aa40fee989efdfaa310a54add351
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-23 00:20:15 +0300

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+
+/**
+ * print_str - prints a string
+ * Return: nothing
+ */
+
+
+void print_str(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}

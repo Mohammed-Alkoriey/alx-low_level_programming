b580be90a42755ca29c3a625ca415c97a246c62d
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-23 14:01:46 +0300

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,43 +1,29 @@
 #include "main.h"
+
 /**
- * _pow_recursion - computes the value of x raised to the power of y
- * @x: is the number
- * @y: is the power
- * Return: the calculated number
+ * is_prime_number - calls a function
+ * @n: the number
+ * Return: 1 if prime and 0 if not
  */
-int is_prime_number(int n);
 
-int main(void)
-{
-    int r;
 
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
-}
+
+
 
 int is_prime_number(int n)
 {
-if (n % 2 != 0 || n == 2)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	int x = 2;
+
+	if (x >= n && n > 1)
+	{
+		return (1);
+	}
+	else if (n % x == 0 || n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (is_prime_number(n, x + 1));
+	}
 }

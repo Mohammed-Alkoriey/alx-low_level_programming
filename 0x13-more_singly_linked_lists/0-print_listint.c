9d2709f0769d85fb90f2e7a6df0fedf8c7ce4622
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-25 21:37:09 +0300

    0.fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t x = 0;
 
-	if (h == NULL)
-	{
-		printf("(nil)");
-	}
-	while(h != NULL)
+	while(h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;

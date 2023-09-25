5b65b676036a95b57448bf6bd8c955ad20a55556
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-25 13:35:26 +0300

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the given head's list
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}

f56e9cc66bd2fd355af3e2857d408baa3f48581f
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-25 22:55:46 +0300

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (x == idx - 1 && t != NULL)
 	{
-		new_node->next = t->next;
+		n->next = t->next;
 		t->next = n;
 		return (n);
 	}

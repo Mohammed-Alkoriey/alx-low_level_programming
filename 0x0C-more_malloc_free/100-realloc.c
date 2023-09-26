c0a3ce4916217942bc8ba80379922c7a48ea574f
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-26 22:35:14 +0300

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *arr;
 	unsigned int i;
 
-	else if (new_size == old_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}

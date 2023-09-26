acaa19ef0446727ee15337e1f86b8230843f59d5
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-26 14:16:44 +0300

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,29 +1,52 @@
 #include "main.h"
+
+int palindrome(char *str, int i, int len);
+int len(char *x);
+
 /**
- * is_palindrome - checks if a string is palindrome
+ * is_palindrome - checks if a string is a palindrome
  * @s: the string
- *
- * Return: 1 on success and 0 on not
+ * Return: 1 if it is success, 0 it's not
  */
 int is_palindrome(char *s)
 {
-	int x = 0, i = 0;
-
-	while (s[x])
-	{
-		x++;
-	}
-	x--;
-	if (x == 0)
+	if (*s == 0)
 	{
 		return (1);
 	}
-	while (x > i)
+	return (palindrome(s, 0, len(s)));
+}
+
+/**
+ * len - calculates length of a string
+ * @x: string
+ * Return: the length of the string
+ */
+int len(char *x)
+{
+	if (*x == '\0')
 	{
-		if (s[i++] != s[x--])
-		{
-			return (0);
-		}
+		return (0);
 	}
-	return (1);
+	return (1 + len(x + 1));
+}
+
+/**
+ * palindrome - checks palindrome
+ * @str: string
+ * @itr: iterator
+ * @l: length of the string
+ * Return: 1 if palindrome, 0 if not
+ */
+int palindrome(char *str, int itr, int l)
+{
+	if (str[itr] != str[l - 1])
+	{
+		return (0);
+	}
+	if (itr >= l)
+	{
+		return (1);
+	}
+	return (palindrome(str, itr + 1, l - 1));
 }

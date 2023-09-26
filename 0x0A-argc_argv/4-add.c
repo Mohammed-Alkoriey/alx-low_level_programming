f630dc8843492a58a9aeafdb2ba5af31536413d2
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-09-26 14:43:52 +0300

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-/**
- * _isdigit - checks for digit 0 through 9
- * @c: the digit
- * Return: 1 if c is a digit and 0 otherwise
- */
-
-int _isdigit(int c)
-{
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
-
 
 /**
  * main - starting point of the program
@@ -32,13 +14,20 @@ return (0);
 int main(int argc, char *argv[])
 {
 	int iAdd = 0, i;
-	long unsigned int x;
-	if (argc == 1){
+	unsigned long int x;
+
+	if (argc == 1)
+	{
 		printf("%d\n", 0);
-	}else if(argc > 1){
-		for (i = 0; i < argc; i++){
-			for (x = 0; x < strlen(argv[i]); x++){
-				if(isdigit(argv[i + x]) == 0){
+	}
+	else if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			for (x = 0; x < (int)strlen(argv[i]); x++)
+			{
+				if (isdigit(argv[i][x]) == 0)
+				{
 					printf("Error\n");
 					return (1);
 				}

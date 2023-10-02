dec8832f7b9e257d6aa71886be7a820beda7682a
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-10-03 00:04:27 +0300

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: the name of the file
+ * @text_content: the content of the new file
+ * Return: 1 in success and 0 if not
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fl;
+	ssize_t x = 0, len = 0;
+
+	while (text_content[len])
+	{
+		len++;
+	}
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fl == -1)
+	{
+		return (-1);
+	}
+	if (len)
+	{
+		x = write(fl, text_content, len);
+	}
+	close(fl);
+	return (x == len ? 1 : -1);
+}

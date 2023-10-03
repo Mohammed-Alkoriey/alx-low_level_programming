4dd981e23d4b1c7ede3f1ad038b089209c262fbe
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-10-03 22:55:51 +0300

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,6 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (len)
 	{
 		x = write(fl, text_content, len);
+		if (x == NULL)
+		{
+			return (-1);
+		}
 	}
 	close(fl);
 	return (x == len ? 1 : -1);
